8ac3e5e0c68807a0c494dd6f0cddc21367cdab3e
Adriano Margarin <adrcxs@gmail.com>
2015-03-21 19:01:59 -0300

    Changed setup.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 from setuptools import find_packages
 
 setup(
-    name='dh_carrinho',
+    name='django-shopping-cart',
     version='1.0',
     packages=find_packages(),
     include_package_data=True,
